4b11ac994aaa7c2018282511ba2e9069ba4ab2b0
Oleksiy Ustichenko <aizsargs@gmail.com>
2021-07-01 10:41:57 +0300

    table and configuration
    table and configuration

---
+++ scripts/scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    $("#checkedAll").change(function() {
+        if (this.checked) {
+            $(".checkSingle").each(function() {
+                this.checked=true;
+            });
+        } else {
+            $(".checkSingle").each(function() {
+                this.checked=false;
+            });
+        }
+    });
+
+    $(".checkSingle").click(function () {
+        if ($(this).is(":checked")) {
+            var isAllChecked = 0;
+
+            $(".checkSingle").each(function() {
+                if (!this.checked)
+                    isAllChecked = 1;
+            });
+
+            if (isAllChecked == 0) {
+                $("#checkedAll").prop("checked", true);
+            }
+        }
+        else {
+            $("#checkedAll").prop("checked", false);
+        }
+    });
+});
+
+$("#idForm").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+    var url = form.attr('action');
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(), // serializes the form's elements.
+        success: function(data)
+        {
+            alert(data); // show response from the php script.
+        }
+    });
+
+
+});
+
+
+
